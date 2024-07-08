afd18eabde0e7e387b4f95a88937760283a81b5b
yixin.panyi <panyi@yixin.im>
2024-07-08 16:13:07 +0800

    activity edge to edge

--- app/src/main/java/xyz/panyi/imgpuzzle/MainActivity.kt
+++ app/src/main/java/xyz/panyi/imgpuzzle/MainActivity.kt
@@ -2,6 +2,7 @@ package xyz.panyi.imgpuzzle
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
 
 /**
  *   MainActivity
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
     }
 
